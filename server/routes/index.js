1a92d25970b98942bac24cfbf852bdd7eb2368ca
Bret Borden <bordenb21@gmail.com>
2022-12-09 15:15:43 -0500

    created routes for users and habits

---
+++ server/routes/index.js
@@ -0,0 +1,27 @@
+const { Router } = require('express')
+const controllers = require('../controllers')
+const router = Router()
+
+router.get('/', (req, res) => res.send('This is root!'))
+
+router.post('/user', controllers.createUser)
+
+router.get('/user', controllers.getAllUsers)
+
+router.get('/user/:id', controllers.getUserById)
+
+router.put('/user/:id', controllers.updateUser)
+
+router.delete('/user/:id', controllers.deleteUser)
+
+router.post('/habit', controllers.createHabit)
+
+router.get('/habit', controllers.getAllHabits)
+
+router.get('/habit/:id', controllers.getHabitById)
+
+router.put('/habit/:id', controllers.updateHabit)
+
+router.delete('/habit/:id', controllers.deleteHabit)
+
+module.exports = router
