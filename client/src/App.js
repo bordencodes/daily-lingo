b14e3c9655c5c58101e11073773509ae6144f31a
Bret Borden <bordenb21@gmail.com>
2022-12-08 22:33:11 -0500

    added inital form and list structure in app.js for later styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,28 @@
 import './App.css'
 
 function App() {
-  return <div className="App">Hello!</div>
+  return (
+    <div className="App">
+      <header>
+        <h1>One Word a Day</h1>
+      </header>
+      <div>
+        <form>
+          <div>
+            <label>Word: </label>
+            <input type="text" id="habit" />
+          </div>
+          <button type="submit">Add</button>
+        </form>
+      </div>
+      <main>
+        <h2>Date</h2>
+        <ul>
+          <li> Word: </li>
+        </ul>
+      </main>
+    </div>
+  )
 }
 
 export default App
