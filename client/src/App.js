76b1239ac78ad23a6364d3441e45b476cf0d7177
Bret Borden <bordenb21@gmail.com>
2022-12-10 11:51:24 -0500

    updated the app.js and imported nav bar component

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import './App.css'
-import { BrowserRouter, Route } from 'react-router-dom'
+import Nav from './components/Nav'
 
 function App() {
   return (
     <div className="App">
       <header>
-        <h1>One Word a Day</h1>
+        {/* <h1>One Word a Day</h1> */}
+        <Nav />
       </header>
       <div>
         <form>
