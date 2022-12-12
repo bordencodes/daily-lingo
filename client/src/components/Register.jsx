03f8944fb1bca2880819b529119033b7d2c18d80
Bret Borden <bordenb21@gmail.com>
2022-12-12 15:34:54 -0500

    updated the register component and added css to the register form

--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -29,25 +29,25 @@ const handleUserRegChange = (e) => {
   return (
     <div>
       <div>Register</div>
-      <form>
+      <form className='register-container'>
         <section>
-        <label>First Name: </label>
+        <label></label>
         <input type="text" id="firstName" placeholder="First Name" maxLength="40" onChange={handleUserRegChange}></input>
         </section>
         <section>
-        <label>Last Name: </label>
+        <label></label>
         <input type="text" id="lasttName" placeholder="Last Name" maxLength="40" onChange={handleUserRegChange}></input>
         </section>
         <section>
-        <label>Email: </label>
+        <label></label>
         <input type="text" id="email" placeholder="Email" maxLength="75" onChange={handleUserRegChange}></input>
         </section>
         <section>
-        <label>Password: </label>
+        <label></label>
         <input type="text" id="password" placeholder="Password" onChange={handleUserRegChange}></input>
         </section>
         <section>
-        <label>Confirm Password: </label>
+        <label></label>
         <input type="text" id="confirmPassword" placeholder="Confirm Password" onChange={handleUserRegChange}></input>
         </section>
         <button type="submit">Submit</button>
