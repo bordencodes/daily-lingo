24e719e936d239a58e233bbe33bda5cf9755d88d
Bret Borden <bordenb21@gmail.com>
2022-12-12 20:32:47 -0500

    adjusted navbar and added color scheme to app

--- client/src/components/TrackedItems.jsx
+++ client/src/components/TrackedItems.jsx
@@ -4,7 +4,7 @@ const TrackedItems = ({name, trans, id, date, onDeleteDailyWord}) => (
   <div>
     <div className="tracked-item">
       <span className="tracked-name">{name}</span>
-      <span className="tracked-translation">Translation: {trans}</span>
+      <span className="tracked-translation"><strong>Translation:</strong> {trans}</span>
       <span className="tracked-date">{date}</span>
       <span className="delete-item" onClick={()=> onDeleteDailyWord(id)}>Delete</span>
     </div>
