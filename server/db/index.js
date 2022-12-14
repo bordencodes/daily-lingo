6c30b4c52d7dfd5b9bc08e5427903fffa5d08d1e
Bret Borden <bordenb21@gmail.com>
2022-12-14 12:00:26 -0500

    adjusted variable for production and local URL in index.js

--- server/db/index.js
+++ server/db/index.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose')
 require('dotenv').config()
 
-let MONGODB_URI = process.env.MONGODB_URI || 'mondgodb://localhost/dailylingoDB'
+// let MONGODB_URI = process.env.MONGODB_URI || 'mondgodb://localhost/dailylingoDB'
+
+let MONGODB_URI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/test'
 
 // let MONGODB_URI =
 //   process.env.NODE_ENV === 'production'
