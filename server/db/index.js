37c54918fdde2a6c0f9ad2fd0cb5f4be16c2dce0
Bret Borden <bordenb21@gmail.com>
2022-12-14 11:46:02 -0500

    adjusted index.js to troubleshoot deloyment issue

--- server/db/index.js
+++ server/db/index.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose')
 require('dotenv').config()
 
-// let MONGODB_URI = process.env.MONGODB_URI || 'mondgodb://localhost/dailylingoDB'
+let MONGODB_URI = process.env.MONGODB_URI || 'mondgodb://localhost/dailylingoDB'
 
-let MONGODB_URI =
-  process.env.NODE_ENV === 'production'
-    ? process.env.MONGODB_URI
-    : 'mongodb://127.0.0.1:27017/test'
+// let MONGODB_URI =
+//   process.env.NODE_ENV === 'production'
+//     ? process.env.MONGODB_URI
+//     : 'mongodb://127.0.0.1:27017/test'
 
 mongoose
   .connect(MONGODB_URI)
