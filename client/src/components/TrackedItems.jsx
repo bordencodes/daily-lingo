35c2b39cec38407ccce5ba644b806593ff885dab
Bret Borden <bordenb21@gmail.com>
2022-12-14 10:09:33 -0500

    added proxy request in development server and updated components

--- client/src/components/TrackedItems.jsx
+++ client/src/components/TrackedItems.jsx
@@ -3,8 +3,8 @@ import React from "react";
 const TrackedItems = ({name, trans, id, date, onUpdateDailyWord, onDeleteDailyWord}) => (
   <div className="tracked-item">
     <span >
-      <div className="tracked-name">
-        <input value={name} />
+      <div className="tracked-name">{name}
+        {/* <input value={name} /> */}
       </div>
       <div className="tracked-translation"><strong>Translation:</strong> {trans}</div>
       <div className="tracked-date">{date}</div>
